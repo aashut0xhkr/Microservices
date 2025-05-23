68976a76489eb126f4657d78baf7e6a12d6a97b1
Ashutosh <aashut0xh@gmail.com>
2025-05-23 19:48:10 +0530

    Api Added

--- src/main/java/com/microservices/user/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/microservices/user/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,19 @@
 package com.microservices.user.Exception;
 
-public class GlobalExceptionHandler {
+import com.microservices.user.Payload.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UserDefinedException.class)
+  public ResponseEntity<ApiResponse> handleResourceNotFoundException(UserDefinedException ex){
+      String message=ex.getMessage();
+        ApiResponse build = ApiResponse.builder().message(message).success(true).status(HttpStatus.NOT_FOUND).build();
+        return new ResponseEntity<ApiResponse>(build,HttpStatus.NOT_FOUND);
+    }
 }
