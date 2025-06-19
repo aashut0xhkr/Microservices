566fcfc6ad28637793ff47661f544283db6c220b
Ashutosh <aashut0xh@gmail.com>
2025-06-19 18:16:23 +0530

    3.0

--- src/main/java/com/microservices/user/Entity/User.java
+++ src/main/java/com/microservices/user/Entity/User.java
@@ -1,12 +1,12 @@
 package com.microservices.user.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -19,4 +19,7 @@ public class User {
     String name;
     String email;
     String about;
+
+    @Transient
+    private List<Rating> ratings=new ArrayList<>();
 }
